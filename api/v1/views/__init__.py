15610a4da7a8deea644d412e1bea477674286d06
Jeff Martinez <2408@holbertonschool.com>
2021-05-09 17:49:34 +0000

    importing states

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Module taht start Blueprint"""
-from flask import Blueprint
+"""Module that start Blueprint"""
 
+from flask import Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+from api.v1.views.states import *
+
