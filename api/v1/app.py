68760e1f8a01245126d21f2676a98f78944d240f
James <jmartinez21834@stu.nuc.edu>
2021-05-09 03:29:51 +0000

    fixing tipos

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(code):
+def teardown_appcontext(self):
     """app.teardown_appcontext"""
     storage.close()
 
